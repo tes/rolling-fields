4f83f5fe56671cd941f1b93287f09bdcd8f6c512
danielreibl <daniel.reibl@guidesmiths.com>
2019-03-20 15:16:48 +0100

    Add defaultMappings. Add new tests

--- lib/index.jsx
+++ lib/index.jsx
@@ -1,19 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const DynamicFormBuilder = ({ form = [], mappings = {} }) => (// eslint-disable-line no-unused-vars
+/* eslint-disable react/prop-types */
+export const defaultMappings = {
+  string: ({ name, key }) => (<input name={name} key={key} />),
+  password: ({ name, key }) => (<input name={name} key={key} type="password" />),
+};
+/* eslint-enable react/prop-types */
+
+const DynamicFormBuilder = ({ form, mappings }) => (
   <form>
     {form.map((field, index) => {
-      const { name } = field;
+      const { name, type = 'string' } = field;
       const key = `${name}${index}`;
-      return <input name={name} key={key} />;
+      return mappings[type] ? mappings[type]({ key, ...field }) : <input name={name} key={key} />;
     })}
   </form>
 );
 
 DynamicFormBuilder.propTypes = {
   form: PropTypes.arrayOf(PropTypes.object).isRequired,
-  mappings: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
+  mappings: PropTypes.object, // eslint-disable-line react/forbid-prop-types
+};
+
+DynamicFormBuilder.defaultProps = {
+  mappings: defaultMappings,
 };
 
 export default DynamicFormBuilder;
--- test/unit/dynamic-form-builder.test.jsx
+++ test/unit/dynamic-form-builder.test.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import { assert } from 'chai';
 import Adapter from 'enzyme-adapter-react-16';
@@ -26,5 +27,49 @@ describe('Dynamic form builder', () => {
     );
     assert.include(wrapper.html(), 'input');
     assert.include(wrapper.html(), 'test');
+  });
+
+  it('creates multiple text inputs by default', () => {
+    const form = [
+      { name: 'test' },
+      { name: 'test2' },
+    ];
+    const wrapper = mount(
+      <DynamicFormBuilder form={form} mappings={{}} />,
+    );
+    assert.include(wrapper.html(), 'input');
+    assert.include(wrapper.html(), 'test');
+    assert.include(wrapper.html(), 'test2');
+  });
+
+  it('creates multiple text inputs by default with different types', () => {
+    const form = [
+      { name: 'test' },
+      { name: 'test2', type: 'password' },
+    ];
+    const wrapper = mount(
+      <DynamicFormBuilder form={form} />,
+    );
+
+    const inputs = wrapper.find('input');
+    assert.equal(inputs.length, 2);
+    assert.deepEqual(inputs.at(0).props(), { name: 'test' });
+    assert.deepEqual(inputs.at(1).props(), { name: 'test2', type: 'password' });
+  });
+
+  it('will use the mappings object and call the render function based on the type', () => {
+    const form = [
+      { name: 'test', type: 'custom' },
+    ];
+    const mappings = {
+      custom: ({ key, name }) => (<input key={key} name={name} />),
+    };
+    const wrapper = mount(
+      <DynamicFormBuilder form={form} mappings={mappings} />,
+    );
+
+    const inputs = wrapper.find('input');
+    assert.equal(inputs.length, 1);
+    assert.deepEqual(inputs.at(0).props(), { name: 'test' });
   });
 });
